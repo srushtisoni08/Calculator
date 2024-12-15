e7527a4ff421542736277025630f124d9824e24f
CBTCIP <sonisrushti12@gmail.com>
2024-12-15 16:28:33 +0530

    Modified project

--- calculate.js
+++ calculate.js
@@ -1,24 +1,33 @@
 let display = document.getElementById("display");
-let currentInput = "";
-let operator = null;
-let first = null;
-let second = null;
-function clear(){
-    currentInput = "";
-    operator = null;
-    first = null;
-    second = null;
-    newdisplay();
-}
-function newdisplay(){
-    display.value = currentInput;
-}
-function operate(opr)
+
+function operate(value)
 {
-    display.value = opr;
-    append();
+    if(value == '+/-'){
+        if(display.value.includes('+','-','*','%')){
+            display.value = "Invalid Operation";
+        }
+        else{
+            display.value = (parseFloat(display.value) * -1).toString();
+        }
+    }
+    else{
+        display.value += value;
+    }
 }
-function append()
-{
-    
+function result(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error)
+    {
+        display.value = 'Invalid Operation';
+    }   
+}
+function brackets(){
+    if(display.value.includes('(')){
+        display.value += ')';
+    }
+    else{
+        display.value += '(';
+    }
 }
